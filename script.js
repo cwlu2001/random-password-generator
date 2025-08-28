b2e51b97279f741bf06377a56764cdaa529ffffc
cwlu2001 <118148817+cwlu2001@users.noreply.github.com>
2025-08-28 15:11:31 +0800

    reduce transfer bandwidth

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const lengthSlider = document.querySelector(".pass-length input"),
 options = document.querySelectorAll(".option input"),
-copyIcon = document.querySelector(".input-box span"),
+copyIcon = document.querySelector(".input-box img"),
 passwordInput = document.querySelector(".input-box input"),
 passIndicator = document.querySelector(".pass-indicator"),
 generateBtn = document.querySelector(".generate-btn");
 
-const characters = { // object of letters, numbers & symbols
+const characters = {
     lowercase: "abcdefghijklmnopqrstuvwxyz",
     uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numbers: "0123456789",
@@ -15,45 +15,26 @@ const characters = { // object of letters, numbers & symbols
 const generatePassword = () => {
     let staticPassword = "",
     randomPassword = "",
-    excludeDuplicate = false,
     passLength = lengthSlider.value;
 
-    options.forEach(option => { // looping through each option's checkbox
-        if(option.checked) { // if checkbox is checked
-            // if checkbox id isn't exc-duplicate && spaces
-            if(option.id !== "exc-duplicate" && option.id !== "spaces") {
-                // adding particular key value from character object to staticPassword
-                staticPassword += characters[option.id];
-            } else if(option.id === "spaces") { // if checkbox id is spaces
-                staticPassword += `  ${staticPassword}  `; // adding space at the beginning & end of staticPassword
-            } else { // else pass true value to excludeDuplicate
-                excludeDuplicate = true;
-            }
+    options.forEach(option => {
+        if(option.checked) {
+            staticPassword += characters[option.id];
         }
     });
 
     for (let i = 0; i < passLength; i++) {
-        // getting random character from the static password
         let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
-        if(excludeDuplicate) { // if excludeDuplicate is true
-            // if randomPassword doesn't contains the current random character or randomChar is equal 
-            // to space " " then add random character to randomPassword else decrement i by -1
-            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
-        } else { // else add random character to randomPassword
-            randomPassword += randomChar;
-        }
+        randomPassword += randomChar;
     }
-    passwordInput.value = randomPassword; // passing randomPassword to passwordInput value
+    passwordInput.value = randomPassword;
 }
 
 const upadatePassIndicator = () => {
-    // if lengthSlider value is less than 8 then pass "weak" as passIndicator id else if lengthSlider 
-    // value is less than 16 then pass "medium" as id else pass "strong" as id
     passIndicator.id = lengthSlider.value <= 7 ? "weak" : lengthSlider.value <= 16 ? "medium" : "strong";
 }
 
 const updateSlider = () => {
-    // passing slider value as counter text
     document.querySelector(".pass-length span").innerText = lengthSlider.value;
     generatePassword();
     upadatePassIndicator();
@@ -61,12 +42,10 @@ const updateSlider = () => {
 updateSlider();
 
 const copyPassword = () => {
-    navigator.clipboard.writeText(passwordInput.value); // copying random password
-    copyIcon.innerText = "check"; // changing copy icon to tick
-    copyIcon.style.color = "#4285F4";
-    setTimeout(() => { // after 1500 ms, changing tick icon back to copy
-        copyIcon.innerText = "content_copy";
-        copyIcon.style.color = "#707070";
+    navigator.clipboard.writeText(passwordInput.value);
+    copyIcon.src = "static/check.svg";
+    setTimeout(() => {
+        copyIcon.src = "static/copy.svg";
     }, 1500);
 }
 
